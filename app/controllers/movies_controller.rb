523da5c338e8a50d5b6a4fc600c554afef4fd69a
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-05 02:20:47 -0300

    cria cadastro filme e botao home

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,26 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def allmovies
+  def index
     @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(title: params[:movie][:title],
+              year: params[:movie][:year],
+              synopsis: params[:movie][:synopsis],
+              country: params[:movie][:country],
+              time: params[:movie][:time],
+              director: params[:movie][:director],
+              gender: params[:movie][:gender],)
+    if @movie.save
+      return redirect_to movie_path(@movie.id)
+    end
+
+    render :new
   end
 end
